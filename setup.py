f6bb7bb540a4bbcb223c726c55de5f7947be9108
Caroline Robinson <CRobinsonAtWork@gmail.com>
2023-10-16 11:03:39 -0800

    Playing around with File step headings - don't want to lose what I already have

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.ChangingDivorceOrder',
-      version='2023.09.21',
+      version='2023.10.16',
       description=('A docassemble extension.'),
       long_description='# docassemble.ChangingDivorceOrder\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nCaroline Robinson, CRobinson@akcourts.gov\r\n\r\n',
       long_description_content_type='text/markdown',
